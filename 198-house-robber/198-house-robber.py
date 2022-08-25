8a4905074a413f88213a0f85c4123c928576d539
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-08-25 15:37:46 -0500

    Time: 41 ms (10.32%), Space: 13.5 MB (43.12%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rob(self, nums):
+        
+        rob1, rob2 = 0, 0
+        
+        # [rob1, rob2, n, n+1]
+        
+        for n in nums:
+            temp = max(rob1 + n, rob2)
+            
+            rob1 = rob2
+            
+            rob2 = temp
+        
+        return rob2
