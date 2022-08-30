96bd9205de330fe66fdbf151be72de73958bc3cd
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-08-30 18:58:44 -0500

    Time: 2503 ms (5.04%), Space: 13.6 MB (83.47%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -6,7 +6,8 @@ class Solution(object):
         for hi in range(len(s)):
             counts[s[hi]] += 1
             max_char_n = counts.most_common(1)[0][1]
-            
+            print(counts.most_common(1))
+            print(counts.most_common(1)[0])
             if hi - lo - max_char_n + 1 > k:
                 counts[s[lo]] -= 1
                 lo += 1
