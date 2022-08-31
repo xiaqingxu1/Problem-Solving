0356f600e7a4d0e0e69d6dab6aaf325629d55f5c
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-08-30 21:58:26 -0500

    Time: 29 ms (87.73%), Space: 13.6 MB (87.70%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -12,11 +12,11 @@ class Solution(object):
                 if target > nums[mid] or target < nums[l]:
                     l = mid + 1
                 else:
-                    r = mid - 1
+                    r = mid
             # right sorted portion
             else:
                 if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
+                    r = mid
                 else:
                     l = mid + 1
         return -1
