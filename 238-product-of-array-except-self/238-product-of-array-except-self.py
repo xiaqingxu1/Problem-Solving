4de99923a8d1e880aebf6e369aba11aebe619685
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-08-30 20:04:16 -0500

    Time: 521 ms (6.40%), Space: 24.5 MB (5.26%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -17,18 +17,5 @@ class Solution(object):
         
         
         
-        # res = [1] * len(nums) // array to present each product
-        
-        # prefix = 1
-        # iterate over the nums, for each i, num
-            # assgin prefix value to res[i]
-            # update prefix by mutiplyin num (to be used in next round)
-        
-        # postfix = 1
-        # iterate over the nums[::-1], for each i, num
-            # assign res[i] = postfix * res[i]
-            # update postfix by multiplying current num
-            
-        # return res
-            
+       
         
