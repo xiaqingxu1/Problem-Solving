50053ba1bd3bb14eafb3ac32d3d8c5f06f43b568
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-09-01 19:34:47 -0500

    Time: 80 ms (93.97%), Space: 31.2 MB (55.25%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -7,14 +7,12 @@ class Solution(object):
     def reorderList(self, head):
         
         slow, fast = head, head.next
-        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
         second = slow.next 
         prev = slow.next = None
-        
         while second:
             temp = second.next
             second.next = prev
