d6a79a51eaa378102df457ca5bc41ee15ac66bf8
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-09-04 21:22:05 -0500

    Time: 2004 ms (99.83%), Space: 23.7 MB (97.41%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -1,20 +1,26 @@
 class WordDictionary:
 
     def __init__(self):
-        self.root = {}
-    
-    def addWord(self, word):
-        node = self.root
-        for char in word:
-            node = node.setdefault(char, {})
-        node[None] = None
+        self.collection = defaultdict(set)
 
+    def addWord(self, word):
+        if word:
+            self.collection[len(word)].add(word)
+        
     def search(self, word):
-        def find(word, node):
-            if not word:
-                return None in node
-            char, word = word[0], word[1:]
-            if char != '.':
-                return char in node and find(word, node[char])
-            return any(find(word, kid) for kid in node.values() if kid)
-        return find(word, self.root)
+        options = self.collection[len(word)]
+        if word == '':
+            return False
+        if '.' not in word:
+            return word in options
+        
+        for option in options:
+            match = True
+            
+            for i, char in enumerate(word):                
+                if word[i] != option[i] and word[i] != '.':
+                    match = False
+                    break
+            if match:
+                return True
+        return False
