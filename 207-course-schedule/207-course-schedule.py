891b09dcc7e74671ddb6837f3b64309b9dbf6690
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-09-09 23:23:04 -0500

    Time: 219 ms (16.84%), Space: 17.1 MB (40.65%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,28 +1,45 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        pre = defaultdict(list)
-        after = defaultdict(list)
-        
-        complete = []
-        
-        for req in prerequisites:
-            course = req[0]
-            takeFirst = req[1]
-            
-            pre[course].append(takeFirst)
-            after[takeFirst].append(course)
-        
-        for i in range(numCourses):
-            if i not in pre:
-                complete.append(i)
-        
-        
-        for c in complete:
-            for j in after[c]:
-                pre[j].remove(c)
-                if not pre[j]:
-                    complete.append(j)
-        
-        return len(complete) == numCourses
-                    
-        
+        # dfs
+        preMap = {i: [] for i in range(numCourses)}
+
+        # map each course to : prereq list
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visiting = set()
+
+        def dfs(crs):
+            if crs in visiting:
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
