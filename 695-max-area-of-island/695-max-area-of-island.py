68b1b777f55988b7f62ad3865199ac07590209aa
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-09-21 17:21:28 -0500

    Time: 138 ms (95.20%), Space: 14.2 MB (92.51%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -11,13 +11,13 @@ class Solution:
             dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             
             while stack:
-                r, c = stack.pop(0)
-                grid[r][c] = 0
-                total += 1
+                r, c = stack.pop()
                 
                 for x, y in dirs:
-                    if 0 <= r + x < rows and 0 <= c + y < cols and [r + x, c + y] not in stack and grid[r + x][c + y] == 1: 
+                    if 0 <= r + x < rows and 0 <= c + y < cols and grid[r + x][c + y] == 1: 
                         stack.append([r + x, c + y])
+                        grid[r + x][c + y] = 0
+                        total += 1
             
             return total
         
@@ -25,6 +25,7 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    total = dfs(r, c, 0)
+                    grid[r][c] = 0
+                    total = dfs(r, c, 1)
                     curMax = max(curMax, total)
         return curMax
