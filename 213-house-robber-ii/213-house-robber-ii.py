8b0c61faf6264a0df7f01aaa3bd2a2448ae7244f
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-09-23 12:02:17 -0500

    Time: 68 ms (13.53%), Space: 13.9 MB (23.73%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,16 +1,16 @@
-class Solution(object):
-    def rob(self, nums):
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.robHelp(nums[1:]), self.robHelp(nums[: -1]))
+        
+        
+        
     
-    
-    def helper(self, nums):
+    def robHelp(self, nums):
         rob1, rob2 = 0, 0 
         for n in nums:
-            curr = max(rob1 + n, rob2)
-            rob1 = rob2
-            rob2 = curr
+            temp = max(rob1 + n, rob2)
+            
+            rob1, rob2 = rob2, temp
         
         return rob2
-
- 
         
