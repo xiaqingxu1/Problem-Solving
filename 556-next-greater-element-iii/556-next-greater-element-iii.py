787bde3d1d1dfae38ea90b6aaf0f30abc146eeec
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-09-27 22:16:17 -0500

    Time: 61 ms (19.56%), Space: 13.8 MB (77.98%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.py
+++ 556-next-greater-element-iii/556-next-greater-element-iii.py
@@ -8,17 +8,16 @@ class Solution:
             x = x // 10
             stack.append(rem) # stack = [1, 2]
             if len(stack) >= 2 and stack[-1] < stack[-2]:
+                
                 stack.sort()
+                stack.reverse()
                 i = stack.index(rem)
-                while i < len(stack) and stack[i + 1] == stack[i]:
-                    i += 1
-                i += 1
-                top = stack.pop(i)
-                stack.insert(0, top)
+                last = stack.pop(i - 1)
+                stack.append(last)
                 break
         
         while stack:
-            y = stack.pop(0)
+            y = stack.pop()
             x = x * 10 + y
         
         return x if n < x <= 2**31 -1 else -1
