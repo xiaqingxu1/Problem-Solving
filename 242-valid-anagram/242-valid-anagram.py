436f035ff14994d890161ca330631bb799c377e3
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-09-29 22:33:51 -0500

    Time: 111 ms (23.77%), Space: 15.1 MB (11.67%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,19 +1,5 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        
-        if len(s) != len(t):
-            return False
-        
-        if not len(s) and not len(t):
-            return True
-        
-        str1 = sorted(list(s))
-       
-        str2 = sorted(list(t))
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
 
-        for i in range(len(str1)):
-            if str1[i] != str2[i]:
-                return False
-        
-        return True
+        return sorted(s) == sorted(t)
         
