76bdc74e79c641f5ac02403fc2e495d02bcaf354
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-10-04 22:10:09 -0500

    Time: 113 ms (28.79%), Space: 16.6 MB (99.55%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.py
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.py
@@ -5,69 +5,67 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def addOneRow(self, root: Optional[TreeNode], v: int, d: int) -> Optional[TreeNode]:
-        if d == 1:
-            return TreeNode(v, root, None)
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        if depth == 1:
+            return TreeNode(val, root, None)
 
-        dq = deque()
-        dq.append(root)
-        level = 1
-        while level < d - 1:
-            n = len(dq)
-            for _ in range(n):
-                curr = dq.popleft()
-                if curr.left:
-                    dq.append(curr.left)
-                if curr.right:
-                    dq.append(curr.right)
-            level += 1
+#         dq = deque()
+#         dq.append(root)
+#         level = 1
+#         while level < d - 1:
+#             n = len(dq)
+#             for _ in range(n):
+#                 curr = dq.popleft()
+#                 if curr.left:
+#                     dq.append(curr.left)
+#                 if curr.right:
+#                     dq.append(curr.right)
+#             level += 1
 
-        for _ in range(len(dq)):
-            curr = dq.popleft()
+#         for _ in range(len(dq)):
+#             curr = dq.popleft()
 
-            left = curr.left
-            right = curr.right
+#             left = curr.left
+#             right = curr.right
 
-            new1 = TreeNode(v)
-            new2 = TreeNode(v)
+#             new1 = TreeNode(v)
+#             new2 = TreeNode(v)
 
-            curr.left = new1
-            new1.left = left
+#             curr.left = new1
+#             new1.left = left
 
-            curr.right = new2
-            new2.right = right
+#             curr.right = new2
+#             new2.right = right
 
-        return root
+#         return root
 
  
 
-         # cur = root
-#         d = 1
-#         level = deque([cur])
-#         while d < depth - 1:
-#             n = len(level)
-#             for _ in range(n):
-#                 node = level.popleft()
-#                 if node.left:
-#                     level.append(node.left)
-#                 if node.right:
-#                     level.append(node.right)
-#             d += 1
-            
-#         for _ in range(len(level)):
-#             node = level.popleft()
-#             if node.left:
-#                 left = node.left
-#                 newNodeL = TreeNode(val, left, None)
-#                 node.left = newNodeL
-                
+        d = 1
+        level = deque()
+        level.append(root)
+        while d < depth - 1:
+            n = len(level)
+            for _ in range(n):
+                node = level.popleft()
+                if node.left:
+                    level.append(node.left)
+                if node.right:
+                    level.append(node.right)
+            d += 1
+        print(level)   
+        for _ in range(len(level)):
+            node = level.popleft()
+            left = node.left
+            newNodeL = TreeNode(val, left, None)
+            node.left = newNodeL
 
-#             if node.right:
-#                 right = node.right
-#                 newNodeR = TreeNode(val, None, right)
-#                 node.right = newNodeR
 
-#         return root
+            right = node.right
+            newNodeR = TreeNode(val, None, right)
+            node.right = newNodeR
+
+        return root
                 
                 
         
